341d7f8cf47c7a7e2e885ca8499f3fc7bc585747
gilla5 <156371739+gilla5@users.noreply.github.com>
2024-09-16 19:42:12 -0400

    Update detail.php

--- detail.php
+++ detail.php
@@ -1,1 +1,47 @@
-
+<? function displayElement($element,$i): void{ ?>
+                    <div>
+                        <p><?= $element['title'].' '.$element['content'].' '.$element['author'].' '.$element['date'].' '?></p>
+                        <ul>
+			                <?php
+				                foreach($element['title'] as $title) echo '<li>'.$title.'</li>';
+				                    
+				                    for($i=0;$i<count($blogPost[0]['title']);$i++){
+					                echo '<li>'.$blogPost[0]['title'][$i].'</li>';
+				                    }
+				                    
+			                    ?>
+		                </ul>
+                        <ul>
+			                <?php
+				                foreach($element['content'] as $content) echo '<li>'.$content.'</li>';
+				                    
+				                    for($i=0;$i<count($blogPost[0]['content']);$i++){
+					                echo '<li>'.$blogPost[0]['content'][$i].'</li>';
+				                    }
+				                    
+			                    ?>
+		                </ul>
+                        <ul>
+			                <?php
+				                foreach($element['author'] as $author) echo '<li>'.$author.'</li>';
+				                    
+				                    for($i=0;$i<count($blogPost[0]['author']);$i++){
+					                echo '<li>'.$blogPost[0]['author'][$i].'</li>';
+				                    }
+				                    
+			                    ?>
+		                </ul>
+                        <ul>
+			                <?php
+				                foreach($element['date'] as $date) echo '<li>'.$date.'</li>';
+				                    
+				                    for($i=0;$i<count($blogPost[0]['date']);$i++){
+					                echo '<li>'.$blogPost[0]['date'][$i].'</li>';
+				                    }
+				                    
+			                    ?>
+		                </ul>
+                    </div>
+                }
+                
+            ?>
