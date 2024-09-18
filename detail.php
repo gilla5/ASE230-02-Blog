0a12a0cdda921ad654a5b6794010f1c47544653d
MNance701 <nancem1@mymail.nku.edu>
2024-09-17 22:04:55 -0400

    Update detail.php in SubBranch1

--- detail.php
+++ detail.php
@@ -1,49 +1,15 @@
 <?php 
-require_once('index.php');
-?>
-
-<?php function displayElement($element, $i) { ?>
-        <div>
-            <p><?= $element['title'].' '.$element['content'].' '.$element['author'].' '.$element['date'].' '?></p>
-                <ul>
-			    <?php
-				    foreach($element['title'] as $title) echo '<li>'.$title.'</li>';
-					
-				        for($i=0;$i<count($blogPost[0]['title']);$i++){
-					        echo '<li>'.$blogPost[0]['title'][$i].'</li>';
-				        }
-				                    
-				?>
-		        </ul>
-                <ul>
-				<?php
-				    foreach($element['content'] as $content) echo '<li>'.$content.'</li>';
-				                    
-				        for($i=0;$i<count($blogPost[0]['content']);$i++){
-							echo '<li>'.$blogPost[0]['content'][$i].'</li>';
-				        }
-				                    
-				?>
-		        </ul>
-                <ul>
-			    <?php
-	                foreach($element['author'] as $author) echo '<li>'.$author.'</li>';
-																		
-						for($i=0;$i<count($blogPost[0]['author']);$i++){
-							echo '<li>'.$blogPost[0]['author'][$i].'</li>';
-				        }
-				                    
-                ?>
-		        </ul>
-                <ul>
-				<?php
-	                foreach($element['date'] as $date) echo '<li>'.$date.'</li>';
-				                    
-				        for($i=0;$i<count($blogPost[0]['date']);$i++){
-							echo '<li>'.$blogPost[0]['date'][$i].'</li>';
-				        }
-				                    
-			    ?>
-		        </ul>
-        </div> 
-<?php }
+require('index.php');
+$i=$_GET['post_id'];
+//Function to display an element
+function displayElement($element, $i) { ?>
+	<?php
+		//Changed the element displays to h1
+		echo '<h1>'.$element[$i]['title'].'</h1>';
+		echo '<p>'.$element[$i]['content'].'</p>';	
+		echo '<h3>'.$element[$i]['author'].'</p>';               
+		echo '<h3>'.$element[$i]['date'].'</p>';
+	?>
+<?php } ?>
+<?php
+displayElement($blogPost, $i);
