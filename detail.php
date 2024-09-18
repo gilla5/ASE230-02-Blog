2bb48f8e4630b4f68792fdcec5bb650ba335a974
Malachi Nance <nancem1@mymail.nku.edu>
2024-09-17 22:52:23 -0400

    Add Bootstrap

--- detail.php
+++ detail.php
@@ -46,6 +46,9 @@ $postId = isset($_GET['post_id']) ? intval($_GET['post_id']) : 0;
 <!DOCTYPE html>
 <html>
 <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>Blog Post Details</title>
 </head>
 <body>
