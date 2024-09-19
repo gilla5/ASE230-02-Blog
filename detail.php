7c6abd85ef91e28fb839e0b68510bd21766340a3
Malachi Nance <nancem1@mymail.nku.edu>
2024-09-19 15:34:42 -0400

    added some styling with bootstrap, added array to json file

--- detail.php
+++ detail.php
@@ -45,13 +45,16 @@ $postId = isset($_GET['post_id']) ? intval($_GET['post_id']) : 0;
 
 <!DOCTYPE html>
 <html>
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <title>Blog Post Details</title>
-</head>
-<body>
-    <?php displayPostDetails($blogPosts, $postId); ?>
-</body>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <title>Blog Post Details</title>
+    </head>
+    <body>
+        <div class="container">
+            <?php displayPostDetails($blogPosts, $postId); ?>
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    </body>
 </html>
